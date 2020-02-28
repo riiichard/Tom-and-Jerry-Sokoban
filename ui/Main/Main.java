d57d8aab501eed97f2fbe59b555007d84312b8b2
Richard <50359364+riiichard@users.noreply.github.com>
2020-02-28 01:12:58 -0800

    Update Main.java

--- ui/Main/Main.java
+++ ui/Main/Main.java
@@ -87,30 +87,6 @@ public class Main extends JFrame implements ActionListener{
 
 
     public static void main(String[] args) throws MalformedURLException, IOException{
-//        BufferedReader br = null;
-//
-//        try {
-//            String theURL = "https://www.ugrad.cs.ubc.ca/~cs210/2018w1/welcomemsg.html"; //this can point to any URL
-//            URL url = new URL(theURL);
-//            br = new BufferedReader(new InputStreamReader(url.openStream()));
-//
-//            String line;
-//
-//            StringBuilder sb = new StringBuilder();
-//
-//            while ((line = br.readLine()) != null) {
-//
-//                sb.append(line);
-//                sb.append(System.lineSeparator());
-//            }
-//
-//            System.out.println(sb);
-//        } finally {
-//
-//            if (br != null) {
-//                br.close();
-//            }
-//        }
         Main sokoban = new Main(false,"Tom");
         sokoban.setVisible(true);
     }
